71d1bb74a4309f3d0c2f79644c3afb8e1b9cf0b1
ALI478QWE <107773693+Ali478qwe@users.noreply.github.com>
2025-02-11 02:39:35 +0330

    Update exip.js

--- docs/exip.js
+++ docs/exip.js
@@ -2,14 +2,15 @@ var express = require('express');
 
 const app = express();
 const port = 3000;
-const serverless =require("serverless-http");
+app.enable('trust proxy');
 app.use(express.urlencoded({extended:true}));
 //headers['x-forwarded-for']
 app.get('/',(req,res)=>{
     let userIp = req.ip || req.socket.remoteAddress; 
     res.send(`Your IPv4 address is ${userIp} `);
     
+});
 
-
-module.exports = app;
-module.exports.handler = serverless(app);
+app.listen(port,()=>{
+    console.log(`run http://localhost:${port} `);
+});
